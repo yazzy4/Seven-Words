26f6b758fd55ed8d97fd3f42f876080c9f1c3ca9
yazzy4 <yazburrell@pursuit.org>
2020-08-31 13:12:31 -0400

    first commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Yaz Burrell on 8/31/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
